e83e510b322dde9fc6cb0080f50171cfc8f27c35
Alex Buchkovsky <alex.buchkovsky@gmail.com>
2019-06-07 03:05:00 +0300

    increment version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md') as file:
 
 setup(
     name='signalrc',
-    version='0.0.6',
+    version='0.0.8',
     description='SignalR client for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
